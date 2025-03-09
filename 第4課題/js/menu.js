ade80100dd5c5deef566137eef0e90f59dc65301
Hideto Masuoka <hidemasuoka112@gmail.com>
2025-03-09 13:16:28 +0900

    動きをなめらかにする

--- 第4課題/js/menu.js
+++ 第4課題/js/menu.js
@@ -2,14 +2,7 @@ $(document).ready(function() {
     // ハンバーガーメニューのトグル
     $('.hamburger').on('click', function() {
         $(this).toggleClass('active');
-        
-        $('.header_content ul').slideToggle({
-            duration: 300,
-            easing: 'swing',
-            complete: function() {
-                $(this).toggleClass('active');
-            }
-        });
+        $('.header_content ul').toggleClass('active');
     });
 
     // ナビゲーションリンクのクリック処理
@@ -30,47 +23,26 @@ $(document).ready(function() {
                 // モバイル時はメニューを閉じる
                 if ($(window).width() <= 550) {
                     $('.hamburger').removeClass('active');
-                    $('.header_content ul').slideUp({
-                        duration: 300,
-                        easing: 'swing',
-                        complete: function() {
-                            $(this).removeClass('active');
-                        }
-                    });
+                    $('.header_content ul').removeClass('active');
                 }
             }
         }
     });
 
-    // リサイズ時の処理 - ここが改善ポイント
+    // リサイズ時の処理
     $(window).resize(function() {
         if ($(window).width() > 550) {
             // PCサイズになった時にメニュー表示を完全にリセット
-            $('.header_content ul').css({
-                'display': '',
-                'height': '',
-                'opacity': ''
-            }).removeClass('active');
             $('.hamburger').removeClass('active');
-        } else {
-            // モバイルサイズに戻った時、メニューは閉じた状態にする
-            if (!$('.hamburger').hasClass('active')) {
-                $('.header_content ul').css('display', 'none').removeClass('active');
-            }
+            $('.header_content ul').removeClass('active');
         }
     });
     
-    // 初期状態のセットアップ - 追加
+    // 初期状態のセットアップ
     function setupInitialMenuState() {
         if ($(window).width() <= 550) {
-            $('.header_content ul').css('display', 'none').removeClass('active');
+            $('.header_content ul').removeClass('active');
             $('.hamburger').removeClass('active');
-        } else {
-            $('.header_content ul').css({
-                'display': '',
-                'height': '',
-                'opacity': ''
-            }).removeClass('active');
         }
     }
     
