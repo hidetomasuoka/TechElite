ed807dcbe1b0d55e4c0e1237746a5253ad0a5767
Hideto Masuoka <hidemasuoka112@gmail.com>
2025-04-07 00:46:58 +0900

    aboutもできた

--- 第6回課題/about.php
+++ 第6回課題/about.php
@@ -1,5 +1,7 @@
- <!-- フリーランスサミットとは？セクション -->
-<section class="about" id="about">
+<!-- フリーランスサミット情報セクション -->
+<section class="about-event-container" style="background-image: url('img/about/about.svg'); background-size: cover; background-position: center;">
+    <!-- フリーランスサミットとは？セクション -->
+    <div class="about" id="about">
         <div class="container">
             <div class="section-header">
                 <h1 class="section-title-en">Freelance Summit</h1>
@@ -10,75 +12,77 @@
                 <p class="text-18">各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</p>
             </div>
         </div>
-    </section>
-<!-- イベント開催概要セクション -->
-<section class="event-summary" id="summary">
-    <div class="container">
-        <div class="section-header centered">
-            <div class="section-title-en">Event Summary</div>
-            <div class="subtitle-container-new">
-                <span class="title-line"></span>
-                <h2 class="section-title-jp">イベント開催概要</h2>
-                <span class="title-line"></span>
+    </div>
+
+    <!-- イベント開催概要セクション -->
+    <div class="event-summary" id="summary">
+        <div class="container">
+            <div class="section-header centered">
+                <div class="section-title-en">Event Summary</div>
+                <div class="subtitle-container-new">
+                    <span class="title-line"></span>
+                    <h2 class="section-title-jp">イベント開催概要</h2>
+                    <span class="title-line"></span>
+                </div>
             </div>
-        </div>
-        <div class="summary-content-wrapper">
-            <table class="summary-table">
-                <tbody>
-                    <!-- 開催日程 -->
-                    <tr>
-                        <td class="summary-label-cell"><span class="summary-label">開催日程</span></td>
-                        <td class="summary-content-cell">2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</td>
-                    </tr>
-                    
-                    <!-- 会場 -->
-                    <tr>
-                        <td class="summary-label-cell"><span class="summary-label">会場</span></td>
-                        <td class="summary-content-cell">東京国際フォーラム ホールE2</td>
-                    </tr>
-                    
-                    <!-- イベント内容 -->
-                    <tr>
-                        <td class="summary-label-cell"><span class="summary-label">イベント内容</span></td>
-                        <td class="summary-content-cell">企業ブース出展：60ブース<br>ビジネス系セミナー：12枠</td>
-                    </tr>
-                    
-                    <!-- 対象者 -->
-                    <tr>
-                        <td class="summary-label-cell"><span class="summary-label">対象者</span></td>
-                        <td class="summary-content-cell">経営者 / フリーランス / 独立志望の会社員</td>
-                    </tr>
-                    
-                    <!-- アクセス -->
-                    <tr>
-                        <td class="summary-label-cell"><span class="summary-label">アクセス</span></td>
-                        <td class="summary-content-cell">
-                            <div class="access-container">
-                                <div class="station-info">
-                                    <p class="text-22 font-bold">【最寄駅】</p>
-                                    <ul class="access-list">
-                                        <li class="access-point">
-                                            <span class="access-marker"></span>
-                                            有楽町駅より徒歩1分<br>東京駅より徒歩5分
-                                        </li>
-                                        <li class="access-point">
-                                            <span class="access-marker"></span>
-                                            有楽町線 ： 有楽町駅(B1F地下コンコースにて連絡）<br>
-                                            千代田線 ： 二重橋前駅より徒歩5分/日比谷駅より徒歩7分<br>
-                                            丸ノ内線 ： 銀座駅より徒歩5分<br>
-                                            銀座線 ： 銀座駅より徒歩7分/京橋駅より徒歩7分<br>
-                                            三田線 ： 日比谷駅より徒歩5分
-                                        </li>
-                                    </ul>
+            <div class="summary-content-wrapper">
+                <table class="summary-table">
+                    <tbody>
+                        <!-- 開催日程 -->
+                        <tr>
+                            <td class="summary-label-cell"><span class="summary-label">開催日程</span></td>
+                            <td class="summary-content-cell">2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</td>
+                        </tr>
+                        
+                        <!-- 会場 -->
+                        <tr>
+                            <td class="summary-label-cell"><span class="summary-label">会場</span></td>
+                            <td class="summary-content-cell">東京国際フォーラム ホールE2</td>
+                        </tr>
+                        
+                        <!-- イベント内容 -->
+                        <tr>
+                            <td class="summary-label-cell"><span class="summary-label">イベント内容</span></td>
+                            <td class="summary-content-cell">企業ブース出展：60ブース<br>ビジネス系セミナー：12枠</td>
+                        </tr>
+                        
+                        <!-- 対象者 -->
+                        <tr>
+                            <td class="summary-label-cell"><span class="summary-label">対象者</span></td>
+                            <td class="summary-content-cell">経営者 / フリーランス / 独立志望の会社員</td>
+                        </tr>
+                        
+                        <!-- アクセス -->
+                        <tr>
+                            <td class="summary-label-cell"><span class="summary-label">アクセス</span></td>
+                            <td class="summary-content-cell">
+                                <div class="access-container">
+                                    <div class="station-info">
+                                        <p class="text-22 font-bold">【最寄駅】</p>
+                                        <ul class="access-list">
+                                            <li class="access-point">
+                                                <span class="access-marker"></span>
+                                                有楽町駅より徒歩1分<br>東京駅より徒歩5分
+                                            </li>
+                                            <li class="access-point">
+                                                <span class="access-marker"></span>
+                                                有楽町線 ： 有楽町駅(B1F地下コンコースにて連絡）<br>
+                                                千代田線 ： 二重橋前駅より徒歩5分/日比谷駅より徒歩7分<br>
+                                                丸ノ内線 ： 銀座駅より徒歩5分<br>
+                                                銀座線 ： 銀座駅より徒歩77/京橋駅より徒歩7分<br>
+                                                三田線 ： 日比谷駅より徒歩5分
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="access-map">
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.327466890697!2d139.76169867569623!3d35.67640157259709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs5Zu95Zyf44OV44Kp44O844Op44Og!5e0!3m2!1sja!2sjp!4v1712367358895!5m2!1sja!2sjp" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    </div>
                                 </div>
-                                <div class="access-map">
-                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.327466890697!2d139.76169867569623!3d35.67640157259709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs5Zu95Zyf44OV44Kp44O844Op44Og!5e0!3m2!1sja!2sjp!4v1712367358895!5m2!1sja!2sjp" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </section>
