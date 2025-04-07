8e5fdc4907db53a55fb802feee5985531a534e64
Hideto Masuoka <hidemasuoka112@gmail.com>
2025-04-08 01:01:20 +0900

    リファクタリング

--- 第6回課題/header.php
+++ 第6回課題/header.php
@@ -3,61 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FREELANCE SUMMIT - 日本最大規模のフリーランスイベント</title>
-    <!-- Google Fonts - Roboto と Noto Sans JP -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <title>training_6</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-        <!-- メインビジュアル部分 -->
-        <div class="main-visual">
-            <div class="container">
-                <div class="main-visual-content">
-                    <!-- ロゴ -->
-                    <div class="stocksun-logo">
-                        <img src="img/stocksun_logo.png" alt="StockSun">
-                    </div>
-        
-                    <!-- サブタイトル -->
-                    <h1 class="sub-title">日本最大規模のフリーランスイベント</h1>
-
-                    <!-- タイトル画像 -->
-                    <div class="main-title">
-                        <img src="img/title.svg" alt="FREELANCE SUMMIT">
-                    </div>
-
-                    <!-- 開催日時・場所 -->
-                    <div class="event-info">
-                        <div class="event-date">
-                            <div class="date-main">3/4　 10:00-18:00</div>
-                            <div class="date-sub">Sat</div>
-                        </div>
-                        <div class="event-place">東京国際フォーラム ホールE2</div>
-                    </div>
-
-                    <!-- 申し込みボタン -->
-                    <div class="apply-button">
-                        <a href="#" class="btn-primary">
-                            <img src="img/line-icon.png" alt="LINE" class="line-icon">
-                            事前申し込みはこちら（無料）
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- 背景装飾 -->
-            <div class="bg-decoration">
-                <!-- 装飾用ポリゴン要素など -->
-            </div>
-        </div>
-
-        <!-- サイドボタン -->
-        <div class="side-button">
-            <a href="#">
-                <span>申し込みはこちら</span>
-                <img src="img/line-icon.png" alt="LINE" class="line-icon">
-            </a>
-        </div>
-    </header>
+    <div class="container" >
+       <div class="header"></div>
+       <div class="sub-title">日本最大規模のフリーランスイベント</div>
+       <div class="kv"></div>
+       <div class="kv_sp"></div>
+       <div class="schedule"></div>
+        <div class="spot">東京国際フォーラム ホールE2</div>
+        <div class="button"><a href="#"><img src="./img/button.png"  alt="LINELogo"></a></div>
+        <div class="hexagon header_1"><img src="./img/hexagon_header_1.png" alt="heared_hexgon"></div>  
+        <div class="hexagon header_2"><img src="./img/hexagon_header_2.png" alt="heared_hexgon"></div>  
+        <div class="hexagon header_3"><img src="./img/hexagon_header_3.png" alt="heared_hexgon"></div> 
+        <div class="hexagon header_4"><img src="./img/hexagon_header_4.png" alt="heared_hexgon"></div>  
+        <div class="hexagon header_5"><img src="./img/hexagon_header_5.png" alt="heared_hexgon"></div> 
+        <div class="hexagon header_6"><img src="./img/hexagon_header_6.png" alt="heared_hexgon"></div> 
+   </div>
